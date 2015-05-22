d0b8728a70efe5d79339dfc4b546b86ba54be9a1
Yosuke ONOUE <onoue@likr-lab.com>
2015-05-22 16:31:30 +0900

    Silent bin

--- bin/post.sh
+++ bin/post.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST -d "{\"timestamp\": {\".sv\": \"timestamp\"}, \"temperature\": $1}" 'https://vizlab-thermometer.firebaseio.com/records.json'
+curl -s -X POST -d "{\"timestamp\": {\".sv\": \"timestamp\"}, \"temperature\": $1}" 'https://vizlab-thermometer.firebaseio.com/records.json' > /dev/null

334178e037f87b6767ff540f2b1159af57c83223
Yosuke ONOUE <onoue@likr-lab.com>
2015-06-14 12:08:54 +0900

    Replace d3 (WIP)

--- src/index.js
+++ src/index.js
@@ -1,59 +1,48 @@
 import angular from 'angular';
 import angularfire from 'angularfire';
-import d3 from 'd3';
-import downloadable from 'd3-downloadable';
+import shinsekai from 'shinsekai';
 import Firebase from 'firebase';
-import render from './render';
 
-angular.module('vizlab-thermometer', [angularfire])
+angular.module('vizlab-thermometer', [angularfire, shinsekai])
   .factory('records', ($firebaseArray) => {
-    const ref = new Firebase('https://vizlab-thermometer.firebaseio.com/records');
+    const ref = new Firebase('https://vizlab-thermometer.firebaseio.com/records')
+      .limitToLast(10);
     return $firebaseArray(ref);
   })
-  .directive('temperatureChart', (records) => {
+  .directive('temperatureChart', (Scale, records) => {
     return {
       restrict: 'E',
-      link: (scope, element) => {
-        const zoom = d3.behavior.zoom(),
-              root = d3.select(element[0])
-                .style({
-                  position: 'absolute',
-                  left: '10px',
-                  right: '10px',
-                  top: '10px',
-                  bottom: '10px'
-                }),
-              svg = root.append('svg')
-                .datum(records)
-                .style({
-                  width: '100%',
-                  height: '100%'
-                })
-                .call(downloadable().filename('chart')),
-              draw = () => {
-                const now = new Date();
-                svg.transition()
-                  .delay(300)
-                  .duration(500)
-                  .call(render({
-                    width: element[0].clientWidth,
-                    height: element[0].clientHeight,
-                    timeDomain: [now - 864000000, +now],
-                    temperatureDomain: [0, 40],
-                    zoom
-                  }));
-              };
-        let loaded = false;
+      template: `
+        <svg width="900" height="900">
+          <g>
+            <circle ng-repeat="record in chart.records"
+                ss-cx="chart.xScale.scale(record.timestamp)"
+                ss-cy="chart.yScale.scale(record.temperature)"
+                r="5"/>
+          </g>
+          <g ss-axis="'left'" ss-ticks="10" ss-scale="chart.yScale"
+              transform="translate(50,50)"/>
+          <g ss-axis="'bottom'" ss-ticks="10" ss-scale="chart.xScale"
+              ss-format="chart.timeFormat"
+              transform="translate(50,850)"/>
+        </svg>
+      `,
+      controllerAs: 'chart',
+      controller: class Chart {
+        constructor() {
+          const now = new Date();
+          this.records = records;
+          this.xScale = new Scale()
+            .domain(now - 86400000, now)
+            .range(0, 800);
+          this.yScale = new Scale()
+            .domain(0, 50)
+            .range(800, 0);
+        }
 
-        records.$loaded().then(() => {
-          loaded = true;
-          draw();
-        });
-        records.$watch(() => {
-          if (loaded) {
-            draw();
-          }
-        });
+        timeFormat(t) {
+          return new Date(t).toLocaleFormat('%m/%d %H:%M');
+        }
       }
     };
   });
